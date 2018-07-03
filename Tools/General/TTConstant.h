e34f26812fc4fbae063c99f49f30641232aa1431
唐博文 <tangbowen@tangbowens-mac-mini.local>
2018-07-03 10:14:08 +0800

    1

--- Tools/General/TTConstant.h
+++ Tools/General/TTConstant.h
@@ -18,11 +18,11 @@
 #import "UIFont+TTSystem.h"
 #import "Masonry.h"
 #import "YYModel.h"
+#import "TTPlaceholderTextView.h"
 
 #define tk_iOS_10_Above    ([UIDevice currentDevice].systemVersion.floatValue>=10.0f)
 #define tk_iOS_9_Above     ([[UIDevice currentDevice].systemVersion floatValue]>=9.0)
 #define tk_iOS_8_Above     ([[UIDevice currentDevice].systemVersion floatValue]>=8.0)
-
 #pragma mark 具体项目中的使用到的类
 
 @interface TTConstant : NSObject
